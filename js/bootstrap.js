e00630807b05a439ca59c6e2cc2e305278c6f0e0
silvangold <silvani1@msu.edu>
2017-04-11 19:28:01 -0400

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -4,19 +4,6 @@
  * Licensed under the MIT license
  */
 
-// ===== Scroll to Top ==== 
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
-});
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
 
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
